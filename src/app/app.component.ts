fd741ee583f2af330b8e4ef9505191833bb468ae
Marcos <pastormarcosgpires@gmail.com>
2021-06-03 17:21:45 -0300

    teste de criacao de branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EDIFICIO AVENIDA PAULISTA';
+  title = 'SALA 103 - SLT';
   //item:number;
   //quantidade:number;
   //destino:string;
